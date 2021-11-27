6ba0284e1876b3854a8f7a1a45c2f2a51065faaa
Alfredo Ucendo <alfredo.ucar@gmail.com>
2021-11-27 13:32:51 +0100

    add scoreboard and team names

--- lib/screens/current_game.dart
+++ lib/screens/current_game.dart
@@ -24,32 +24,59 @@ class _CurrentGame extends State<CurrentGame>
   @override
   void initState() {
     super.initState();
-    _ticker = this.createTicker((elapsed) {
-      setState(() {
-        _elapsed = _game.getElapsed();
-      });
-    });
-    _ticker.start();
+    // _ticker = this.createTicker((elapsed) {
+    //   setState(() {
+    //     _elapsed = _game.getElapsed();
+    //   });
+    // });
+    // _ticker.start();
   }
 
-  @override
-  void dispose() {
-    _ticker.dispose();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   _ticker.dispose();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
+    var teamNameStyle = TextStyle(fontSize: 18);
+    var scoreBoardStyle = TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          title: Text("${_game.yourTeamName} VS ${_game.opponentTeamName}"),
+          title: Text("Game"),
         ),
-        body: Center(
-            child: Column(
+        body: Column(
           children: [
-            Text("${_elapsed.inMinutes}:${_elapsed.inSeconds.remainder(60)}"),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text(
+                    _game.yourTeamName,
+                    style: teamNameStyle,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          "12-11", // TODO: replace with actual score
+                          style: scoreBoardStyle,
+                        ),
+                        Text(
+                            "${_elapsed.inMinutes}:${_elapsed.inSeconds.remainder(60)}"),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    _game.opponentTeamName,
+                    style: teamNameStyle,
+                  ),
+                ],
+              ),
+            ),
           ],
-        )));
+        ));
   }
 }
