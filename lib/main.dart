f6adee6a70dea989cb731254ab82478f0dfad7f9
Alfredo Ucendo <alfredo.ucar@gmail.com>
2021-12-21 23:05:44 +0100

    initialize firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:ultiplay/screens/current_game.dart';
 import 'package:ultiplay/screens/home.dart';
 import 'package:ultiplay/screens/new_game.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Ultiplay());
 }
 
