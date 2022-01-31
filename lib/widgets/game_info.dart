1ff3a56382ed7960d6d2bb12004ef5ab91d0a35d
Alfredo Ucendo <alfredo.ucar@gmail.com>
2022-01-31 23:02:55 +0100

    remove unused imports

--- lib/widgets/game_info.dart
+++ lib/widgets/game_info.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:ultiplay/models/checkpoint.dart';
 import 'package:ultiplay/models/entities/checkpoint_entry.dart';
 import 'package:ultiplay/models/game.dart';
 import 'package:ultiplay/states/played_games.dart';
